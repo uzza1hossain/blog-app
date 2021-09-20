6a09c5c890fa17edb29abff6cd18b318a6775b28
Uzzal Hossain <de3dm3nu11@gmail.com>
2021-09-20 12:41:24 +0600

    forms and user accounts

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import SingUpView
+
+urlpatterns = [
+    path('singup/', SingUpView.as_view(), name='singup'),
+    
+]
--- blog/tests.py
+++ blog/tests.py
@@ -27,6 +27,10 @@ class BlogTest(TestCase):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
 
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -48,6 +52,30 @@ class BlogTest(TestCase):
         self.assertEqual(no_res.status_code, 404)
         self.assertContains(res, 'A good title')
         self.assertTemplateUsed(res, 'post_detail.html')
+
+
+    def test_post_create_view(self):
+        res = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user.id,
+        })
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().body, 'New text')
+    
+
+    def test_post_update_view(self):
+        res = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+            })
+        self.assertEqual(res.status_code, 302)
+
+    def test_post_delete_view(self):
+        res = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(res.status_code, 302)
+    
         
         
 
